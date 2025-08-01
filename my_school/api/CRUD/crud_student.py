da70ae84213d38f79901918e710bcb2a1c4f4bd1
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-01 20:22:22 +0700

    add create endpoint student

--- my_school/api/CRUD/crud_student.py
+++ my_school/api/CRUD/crud_student.py
@@ -1,12 +1,25 @@
+from typing import Sequence
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.model import Student
+from core.schemas.schema_student import StudentCreate
 
 
 async def get_students(
     session: AsyncSession,
-) -> list[Student]:
+) -> Sequence[Student]:
     stmt = select(Student).order_by(Student.id)
     students = await session.scalars(stmt)
-    return students
+    return students.all()
+
+
+async def create_student(
+    session: AsyncSession,
+    data_student: StudentCreate,
+) -> Student:
+    student = Student(**data_student.model_dump())
+    session.add(student)
+    await session.commit()
+    await session.refresh(student)
+    return student
