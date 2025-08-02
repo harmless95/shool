c13b0d222488797db9e0875228db4c6788f82ffd
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-02 23:29:49 +0700

    corrected model student

--- my_school/core/model/student.py
+++ my_school/core/model/student.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Date
 
 from core.model.base import Base
-from .students_school import students_object_table
+from .students_subject import students_object_table
 
 if TYPE_CHECKING:
     from .school_subjects import SchoolSubject
@@ -17,6 +17,7 @@ class Student(Base):
     year_of_birth: Mapped[date] = mapped_column(Date, nullable=False)
 
     school_subjects: Mapped[list["SchoolSubject"]] = relationship(
+        "SchoolSubject",
         secondary=students_object_table,
         back_populates="students",
     )
