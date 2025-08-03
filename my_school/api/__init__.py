16b4c8d99f38f4ab05e4b55a92e7ccba206ee4e0
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-03 13:53:11 +0700

    add router day and subject

--- my_school/api/__init__.py
+++ my_school/api/__init__.py
@@ -1,3 +1,9 @@
-__all__ = ("router_student",)
+__all__ = (
+    "router_student",
+    "router_day",
+    "router_subject",
+)
 
 from .student import router as router_student
+from .day_school import router as router_day
+from .subject import router as router_subject
