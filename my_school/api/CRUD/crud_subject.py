1cd1ef5f64b1eb8cbb05b2e747ff745e6afb3649
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-03 21:59:56 +0700

    create get subject by id

--- my_school/api/CRUD/crud_subject.py
+++ my_school/api/CRUD/crud_subject.py
@@ -1,9 +1,29 @@
 from typing import Sequence
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from fastapi import HTTPException, status
 
 from core.model import SchoolSubject
-from core.schemas.schema_school_subject import SchoolSubjectCreate, SchoolSubjectBase
+from core.schemas.schema_school_subject import (
+    SchoolSubjectCreate,
+    SchoolSubjectBase,
+    SchoolSubjectUpdate,
+)
+
+
+async def get_subject(
+    session: AsyncSession,
+    id_subject: int,
+) -> SchoolSubject:
+    stmt = select(SchoolSubject).where(SchoolSubject.id == id_subject)
+    result = await session.scalars(stmt)
+    subject = result.first()
+    if not subject:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="invalid subject by id",
+        )
+    return subject
 
 
 async def get_all(session: AsyncSession) -> Sequence[SchoolSubject]:
