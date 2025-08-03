159aef65b9ab5282ece0316f76076ba841f79b1b
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-03 13:55:49 +0700

    create get all day and create new day

--- my_school/api/day_school.py
+++ my_school/api/day_school.py
@@ -1,17 +1,37 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.model import db_helper
+from .CRUD.crud_day import get_day_all, create_day
+from core.schemas.schema_day import SchemaDayCreate, SchemaDayBase
 
 router = APIRouter(prefix="/day", tags=["Day"])
 
 
-@router.get("/")
+@router.get(
+    "/",
+    response_model=list[SchemaDayBase],
+)
 async def get_all_day(
     session: Annotated[
         AsyncSession,
         Depends(db_helper.session_getter),
     ],
 ):
-    pass
+    return await get_day_all(session=session)
+
+
+@router.post(
+    "/",
+    response_model=SchemaDayBase,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_school_day(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    data: SchemaDayCreate,
+):
+    return await create_day(
+        session=session,
+        data=data,
+    )
