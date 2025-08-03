2ec6bea562f6f957102f84ab7d9ebdcccb82fc72
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-03 13:54:45 +0700

    add check for unique

--- my_school/api/CRUD/crud_student.py
+++ my_school/api/CRUD/crud_student.py
@@ -1,5 +1,5 @@
 from typing import Sequence
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.model import Student
@@ -18,8 +18,19 @@ async def create_student(
     session: AsyncSession,
     data_student: StudentCreate,
 ) -> Student:
-    student = Student(**data_student.model_dump())
-    session.add(student)
-    await session.commit()
-    await session.refresh(student)
-    return student
+    stmt = select(Student).where(
+        and_(
+            Student.name == data_student.name,
+            Student.surname == data_student.surname,
+            Student.year_of_birth == data_student.year_of_birth,
+        )
+    )
+    student_school = await session.scalars(stmt)
+    result = student_school.first()
+    if not result:
+        student = Student(**data_student.model_dump())
+        session.add(student)
+        await session.commit()
+        await session.refresh(student)
+        result = student
+    return result
