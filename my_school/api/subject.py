c01796c8b8e2dccb18557d00b16a7f8cfa31c509
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-03 22:06:09 +0700

    added requests get subject by id and annotated

--- my_school/api/subject.py
+++ my_school/api/subject.py
@@ -2,14 +2,38 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.model import db_helper
-from .CRUD.crud_subject import create_subject, get_all
-from core.schemas.schema_school_subject import SchoolSubjectCreate, SchoolSubjectRead
+from core.model import db_helper, SchoolSubject
+from .CRUD.crud_subject import create_subject, get_all, get_subject, update_subject
+from core.schemas.schema_school_subject import (
+    SchoolSubjectCreate,
+    SchoolSubjectRead,
+    SchoolSubjectUpdate,
+)
+
 
 router = APIRouter(prefix="/subject", tags=["Subject"])
 
 
-@router.get("/", response_model=list[SchoolSubjectRead])
+@router.get(
+    "/{id_subject}/",
+    response_model=SchoolSubjectRead,
+    status_code=status.HTTP_200_OK,
+)
+async def get_subject_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    id_subject: int,
+) -> SchoolSubject:
+    return await get_subject(
+        session=session,
+        id_subject=id_subject,
+    )
+
+
+@router.get(
+    "/",
+    response_model=list[SchoolSubjectRead],
+    status_code=status.HTTP_200_OK,
+)
 async def get_all_subject(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
