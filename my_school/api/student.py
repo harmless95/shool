9ca1211c5d92b28fe9afdf371faf8bee0d93583c
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-03 22:04:42 +0700

    added requests get by id and annotated

--- my_school/api/student.py
+++ my_school/api/student.py
@@ -1,18 +1,45 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.model import db_helper, Student
 from .CRUD.crud_student import (
     get_students as crud_students,
     create_student as crud_student_create,
+    get_student,
+    update_student,
 )
-from core.schemas.schema_student import StudentBase, StudentCreate, StudentRead
+from core.schemas.schema_student import (
+    StudentBase,
+    StudentCreate,
+    StudentRead,
+    StudentRead2,
+    StudentUpdate,
+)
 
 router = APIRouter(prefix="/student", tags=["Student"])
 
 
-@router.get("/", response_model=list[StudentBase])
+@router.get(
+    "/{id_student}/",
+    response_model=StudentRead2,
+    status_code=status.HTTP_200_OK,
+)
+async def get_student_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    id_student: int,
+) -> Student:
+    return await get_student(
+        session=session,
+        id_student=id_student,
+    )
+
+
+@router.get(
+    "/",
+    response_model=list[StudentRead2],
+    status_code=status.HTTP_200_OK,
+)
 async def get_students(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
