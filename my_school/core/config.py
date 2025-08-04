fca16863f859fff074c24fcc1822e6cb0a0f9817
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-04 18:20:47 +0700

    create config gunicorn and config logging

--- my_school/core/config.py
+++ my_school/core/config.py
@@ -1,10 +1,34 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel, PostgresDsn
+
+# fmt: off
+LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+# fmt: on
 
 
 class RunConfig(BaseModel):
     port: int = 8000
     host: str = "0.0.0.0"
+
+
+class GunicornConfig(BaseModel):
+    port: int = 8000
+    host: str = "0.0.0.0"
+    workers: int = 1
+    timeout: int = 900
+
+
+class LoggingConfig(BaseModel):
+    log_level: Literal[
+        "debag",
+        "info",
+        "warning",
+        "error",
+        "critical",
+    ] = "info"
+    log_format: str = LOG_DEFAULT_FORMAT
 
 
 class DataBaseConfig(BaseModel):
@@ -28,6 +52,8 @@ class Setting(BaseSettings):
     )
     db: DataBaseConfig
     run: RunConfig = RunConfig()
+    gunicorn: GunicornConfig = GunicornConfig()
+    logging: LoggingConfig = LoggingConfig()
     prefix: Prefix = Prefix()
 
 
