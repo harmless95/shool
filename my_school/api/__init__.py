c237b714fce25aaaeb888c71ed6670ee0730e6e5
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-07 20:46:07 +0700

    add routers_v2

--- my_school/api/__init__.py
+++ my_school/api/__init__.py
@@ -1,9 +1,6 @@
-__all__ = (
-    "router_student",
-    "router_day",
-    "router_subject",
-)
+__all__ = ("router_student", "router_day", "router_subject", "routers_v2")
 
 from api.api_v1.student import router as router_student
 from api.api_v1.day_school import router as router_day
 from api.api_v1.subject import router as router_subject
+from .api_v2 import router as routers_v2
