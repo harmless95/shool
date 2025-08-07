ac27cdba0323de01b39acd05f5dc372d01e3d207
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-07 20:44:40 +0700

    add register router

--- my_school/api/api_v2/auth.py
+++ my_school/api/api_v2/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from api.dependencies.auntification.backend import authentication_backend
 from core.config import setting
+from core.schemas.users import UserRead, UserCreate
 
 from .fastapi_users_router import fastapi_users_router
 
@@ -10,6 +11,15 @@ router = APIRouter(
     tags=["Auth"],
 )
 
+# login
+# logout
 router.include_router(
     router=fastapi_users_router.get_auth_router(authentication_backend)
+)
+# register
+router.include_router(
+    router=fastapi_users_router.get_register_router(
+        UserRead,
+        UserCreate,
+    )
 )
