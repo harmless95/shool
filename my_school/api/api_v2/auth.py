b854340c7dd3f3ab3d943a4332b9a4ebe614d79b
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-08-07 19:31:20 +0700

    include auth router from fastapi-users

--- my_school/api/api_v2/auth.py
+++ my_school/api/api_v2/auth.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
 
+from api.dependencies.auntification.backend import authentication_backend
 from core.config import setting
+
+from .fastapi_users_router import fastapi_users_router
 
 router = APIRouter(
     prefix=setting.api.v2.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    router=fastapi_users_router.get_auth_router(authentication_backend)
 )
